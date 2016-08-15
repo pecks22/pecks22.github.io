4965ac3334084b74543f2fe1d0d54e8fcb988da9
pecks22 <becky@Beckys-MacBook-Air.local>
2016-08-15 17:59:37 -0400

    reverting back

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-$(document).ready(function(){$("#burger").click(function(){$("#hiddendiv").slideToggle()}),$(".row").click(function(){$(".row").find(".wrapper").removeClass("clicked"),$(this).find(".wrapper").addClass("clicked"),$(".clicked").slideToggle()})});
+$(document).ready (function () {
+
+ 
+
+    $('#burger').click( function() {
+  $('#hiddendiv').slideToggle();
+
+  });
+
+ 
+
+    $('.row').click(function () {
+
+      $('.row').find('.wrapper').removeClass('clicked');
+      $(this).find('.wrapper').addClass('clicked');
+      $('.clicked').slideToggle();
+    
+
+  });
+    
+      });
