b08e6e628fb81c426bfe1c97305ff0752a7e912e
pecks22 <becky@Beckys-Air.home>
2016-08-14 20:50:24 -0400

    final project work

--- js/app.js
+++ js/app.js
@@ -15,18 +15,8 @@ $(document).ready (function () {
 
       $('.row').find('.wrapper').removeClass('clicked');
       $(this).find('.wrapper').addClass('clicked');
-      $('clicked').find('.wrapper').slideToggle();
-
-
-     /* $(this).find('.wrapper').addClass('clicked');
-      console.log('clicked')
-
-      :not(this).removeClass('clicked');
-      $('clicked').find('.wrapper').slideToggle();
-
-    /*$('.wrapper').css('display','none');
-    $(this).find('.wrapper').slideToggle();
-    }); */
+      $('.clicked').slideToggle();
+    
 
   });
     
